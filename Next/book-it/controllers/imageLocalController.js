f94c377392fd9fe3350a61daf91d690f8315bad0
nductoan <toanbeo090400@gmail.com>
2022-11-09 16:52:02 +0700

    image ong done

--- Next/book-it/controllers/imageLocalController.js
+++ Next/book-it/controllers/imageLocalController.js
@@ -1,5 +1,5 @@
 import ImageLocal from "../models/imageLocal";
-
+import fs from "fs";
 const allImageOne = async (req, res) => {
   try {
     const images = await ImageLocal.find();
@@ -37,39 +37,50 @@ const newImageOne = async (req, res) => {
   }
 };
 
-// const updateImage = async (req, res) => {
-//   try {
-//     const book = await Book.findByIdAndUpdate(req.query.id, req.body, {
-//       new: true,
-//       runValidators: true,
-//     });
-//     res.status(200).json({
-//       success: true,
-//       message: "updateBook thành công",
-//       book,
-//     });
-//   } catch (error) {
-//     res.status(400).json({
-//       success: false,
-//       error,
-//     });
-//   }
-// };
+const updateImageOne = async (req, res) => {
+  try {
+    // tìm và xóa image cũ
+    const newImage = await ImageLocal.findById(req.query.id);
+    const path = `public/image-one/${newImage.image.filename}`;
+    fs.unlink(path, (err) => {
+      return console.log(err);
+    });
+    // thêm ảnh mới và lưu
+    newImage.image = req.file;
+    await newImage.save();
 
-// const deleteImage = async (req, res) => {
-//   try {
-//     const book = await Book.findByIdAndDelete(req.query.id);
-//     res.status(200).json({
-//       success: true,
-//       message: "deleteBook thành công",
-//       book,
-//     });
-//   } catch (error) {
-//     res.status(400).json({
-//       success: false,
-//       error,
-//     });
-//   }
-// };
+    res.status(200).json({
+      success: true,
+      message: "updateImage thành công",
+      newImage,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
+
+const deleteImageOne = async (req, res) => {
+  try {
+    // const image = await ImageLocal.findById(req.query.id);
+    const image = await ImageLocal.findByIdAndDelete(req.query.id);
+    const path = `public/image-one/${image.image.filename}`;
+    fs.unlink(path, (err) => {
+      return console.log(err);
+    });
+    res.status(200).json({
+      success: true,
+      message: "deleteImage thành công",
+      image,
+    });
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      error,
+    });
+  }
+};
 // ----------- Many -----------------
-export { allImageOne, newImageOne, getImage, updateImage, deleteImage };
+export { allImageOne, newImageOne, updateImageOne, deleteImageOne };
--- Next/book-it/pages/api/imageLocal/single/[id].js
+++ Next/book-it/pages/api/imageLocal/single/[id].js
@@ -2,17 +2,46 @@ import nextConnect from "next-connect";
 
 import db from "../../../../config/dbConnect";
 import {
-  getImage,
-  updateImage,
-  deleteImage,
+  updateImageOne,
+  deleteImageOne,
 } from "../../../../controllers/imageLocalController";
 
 db();
 
+import multer from "multer";
+import { v4 as uuidv4 } from "uuid";
+import path from "path";
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "public/image-one"); // nơi lưu ảnh
+  },
+  filename: function (req, file, cb) {
+    cb(null, uuidv4() + "-" + Date.now() + path.extname(file.originalname));
+  },
+});
+
+const fileFilter = (req, file, cb) => {
+  const allowedFileTypes = ["image/jpeg", "image/jpg", "image/png"];
+  if (allowedFileTypes.includes(file.mimetype)) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+// *** upload file cần thiết
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
+const upload = multer({ storage, fileFilter });
+const image = upload.single("image");
+
 const handler = nextConnect();
 
-handler.get(getImage);
-handler.patch(updateImage);
-handler.delete(deleteImage);
+handler.use(image);
+handler.patch(updateImageOne);
+handler.delete(deleteImageOne);
 
 export default handler;
